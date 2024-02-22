eb81c45e119c6cc40b979312234d623e883efb86
charlieroth <charlieroth4@icloud.com>
2024-02-22 17:30:13 +0100

    init
    Two endpoints:
    * Send data to vault
    * Get data from vault

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,33 @@
 import Vapor
+import Foundation
+
+struct VaultData: Content {
+    let key: String
+    let value: String
+}
+
+struct PostResponse: Content {
+    let key: String
+}
+
+var vault: [String: VaultData] = [:]
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    app.get(":key") { req async throws -> VaultData in
+        guard let key = req.parameters.get("key") else {
+            throw Abort(.internalServerError)
+        }
+        
+        guard let data = vault[key] else {
+            throw Abort(.noContent)
+        }
+        
+        return data
     }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    
+    app.post { req async throws -> PostResponse in
+        let data = try req.content.decode(VaultData.self)
+        vault[data.key] = data
+        return PostResponse(key: data.key)
     }
 }
